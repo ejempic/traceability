743711b3d03cd3a7372ca2d1db69835bcf182e8b
Earl <empicearl@gmail.com>
2021-06-13 11:31:05 +0800

    add more unit test

--- tests/Feature/CreateProductTest.php
+++ tests/Feature/CreateProductTest.php
@@ -10,28 +10,45 @@ use Illuminate\Foundation\Testing\WithoutMiddleware;
 use Illuminate\Support\Facades\DB;
 use Tests\TestCase;
 
-class LoginTest extends TestCase
+class CreateProductTest extends TestCase
 {
-//    use WithoutMiddleware; // use the trait
+    use WithoutMiddleware; // use the trait
     /**
      * A basic feature test example.
      *
      * @return void
      */
-    public function testLogin()
+    public function testCreateProduct()
     {
-
         DB::beginTransaction();
         $user = factory(User::class)->create([
             'password' => bcrypt($password = 'i-love-laravel'),
         ]);
+        $user->assignRole(stringSlug('super-admin'));
+        $user->markEmailAsVerified();
 
         $response = $this->post('/login', [
             'email' => $user->email,
             'password' => $password,
         ]);
 
-        $response->assertRedirect('/home');
+        $this->assertAuthenticatedAs($user);
+
+        $response = $this->get(route("product.create"));
+//
+        $response->assertStatus(200);
+
+        $faker = Factory::create();
+        $create = [
+            'name' => $faker->name,
+            'description' => $faker->paragraph(3),
+        ];
+
+
+        $response = $this->post(route('product.store'), $create);
+        $response->assertStatus(302);
+
         DB::rollBack();
+
     }
 }
