f316edbeea3581418141c9d0054508a0767270f7
frightnight <jasper.garcera@gmail.com>
2021-07-20 21:50:14 +0800

    admin update

---
+++ app/Http/Controllers/LoanApplicationDetailController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\LoanApplicationDetail;
+use Illuminate\Http\Request;
+
+class LoanApplicationDetailController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\LoanApplicationDetail  $loanApplicationDetail
+     * @return \Illuminate\Http\Response
+     */
+    public function show(LoanApplicationDetail $loanApplicationDetail)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\LoanApplicationDetail  $loanApplicationDetail
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(LoanApplicationDetail $loanApplicationDetail)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\LoanApplicationDetail  $loanApplicationDetail
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, LoanApplicationDetail $loanApplicationDetail)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\LoanApplicationDetail  $loanApplicationDetail
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(LoanApplicationDetail $loanApplicationDetail)
+    {
+        //
+    }
+}
---
+++ database/factories/LoanApplicationDetailFactory.php
@@ -0,0 +1,12 @@
+<?php
+
+/** @var \Illuminate\Database\Eloquent\Factory $factory */
+
+use App\LoanApplicationDetail;
+use Faker\Generator as Faker;
+
+$factory->define(LoanApplicationDetail::class, function (Faker $faker) {
+    return [
+        //
+    ];
+});
