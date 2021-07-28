e8fa2c918937f3d690fd8de3dac6946c92c2ec67
Angela Nicole Belen <anbelen.official@gmail.com>
2021-07-28 15:51:05 +0800

    update loan master

--- resources/views/loan/master.blade.php
+++ resources/views/loan/master.blade.php
@@ -83,6 +83,7 @@
             </ul>
 
         </div>
+        <div class="version"><div class="long">Agrabah Finance v.01</div><div class="short">v.01</div></div>
     </nav>
 
     <div id="page-wrapper" class="gray-bg">
@@ -93,6 +94,9 @@
                     <a class="navbar-minimalize minimalize-styl-2 btn btn-primary " href="#"><i class="fa fa-bars"></i> </a>
 
                     {{-- slot for search bar --}}
+                    <div class="mobile-logo">
+                        <img alt="image" class="img-fluid" src="{{ asset('images/logo.png') }}"/>
+                    </div>
 
                 </div>
                 <ul class="nav navbar-top-links navbar-right">
@@ -119,12 +123,9 @@
         </div>
 
         <div class="footer">
-            <div class="float-right">
-                <strong></strong>
-            </div>
-            <div>
-                <strong>Copyright</strong> {{ subdomain_title('ucwords') }}
-                <strong></strong>
+            <div class="copyright">
+                <p>Powered by Agrabah Ventures</p>
+                <p>Powered by Agrabah Loan</p>
             </div>
         </div>
 
