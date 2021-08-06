dda4808b056d65a2a3c0995ecf3c0250fb3691f8
Earl <empicearl@gmail.com>
2021-08-06 13:23:52 +0800

    Diffirentiate spot market community leader to farmer

--- resources/views/wharf/menu.blade.php
+++ resources/views/wharf/menu.blade.php
@@ -2,9 +2,9 @@
     <a href="{!! route('home') !!}"><i class="fa fa-tachometer"></i> <span class="nav-label">Home</span></a>
 </li>
 
-@if(auth()->user()->can('read-spot-market')  && isCommunityLeader())
+@if(auth()->user()->can('read-spot-market') && isCommunityLeader())
     <li class="{{ (request()->is('spot-market*')) ? 'active' : '' }}">
-        <a href="#"><i class="fa fa-list-alt"></i> <span class="nav-label">Spot Market</span><span class="fa arrow"></span></a>
+        <a href="#"><i class="fa fa-list-alt"></i> <span class="nav-label">My Spot Market</span><span class="fa arrow"></span></a>
         <ul class="nav nav-second-level collapse">
             <li class="{{ (request()->is('spot-market')) ? 'active' : '' }}"><a href="{!! route('spot-market.index') !!}">My List</a></li>
 
@@ -14,7 +14,7 @@
         </ul>
     </li>
 @endif
-@if(auth()->user()->can('read-spot-market'))
+@if(auth()->user()->can('browse-spot-market'))
     <li class="{{ (request()->is('spot-market*')) && !(request()->is('spot-market-cart')) ? 'active' : '' }}">
         <a href="{!! route('spot-market.index') !!}"><i class="fa fa-list"></i> <span class="nav-label">Spot Market</span></a>
     </li>
