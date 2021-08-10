d4b99892994864d737e3908e2e126cf528203cc9
Earl <empicearl@gmail.com>
2021-08-10 16:43:35 +0800

    test

--- resources/views/wharf/menu.blade.php
+++ resources/views/wharf/menu.blade.php
@@ -19,6 +19,7 @@
         <a href="{!! route('spot-market.index') !!}"><i class="fa fa-list"></i> <span class="nav-label">Spot Market</span></a>
     </li>
 @endif
+
 {{--@if(auth()->user()->can('buy-spot-market'))--}}
 {{--    <li class="{{ (request()->is('spot-market-cart')) ? 'active' : '' }}">--}}
 {{--        <a href="{!! route('spot-market.cart') !!}"><i class="fa fa-shopping-cart"></i> <span class="nav-label">Cart</span></a>--}}
