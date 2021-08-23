6db0978acdd2c0e51b79068044503b2141a10d1a
Angela Nicole Belen <anbelen.official@gmail.com>
2021-08-23 23:25:24 +0800

    updated wharf login blade

--- resources/views/wharf/auth/login.blade.php
+++ resources/views/wharf/auth/login.blade.php
@@ -27,7 +27,8 @@
                 <div class="content w-100">
                     <img src="{{ asset('images/logo.png') }}" alt="logo" class="img-fluid d-block mx-auto logo mb-5 d-block d-lg-none">
                     <h1 class="title">Wharf Login</h1>
-                    <small>Log in using your login and password or use social media login to enter</small>
+                    {{--<small>Log in using your login and password or use social media login to enter</small>--}}
+                    <small>Log in using your email and password</small>
 
                     <form method="POST" action="{{ route('login') }}">
                         @csrf
