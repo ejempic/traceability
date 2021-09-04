da157aed1ca7fce5ffd4b9eb605d6d09d68295b3
Earl <empicearl@gmail.com>
2021-09-05 03:03:33 +0800

    remove login error

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -45,7 +45,7 @@ class LoginController extends Controller
      */
     public function showLoginForm()
     {
-        return view(subDomainPath('auth.login'));
+        return view('auth.login');
     }
 
     /**
